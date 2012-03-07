6237c031cbc4d13a377392beb1698217080a83b2
Raul Barroso <ra.barroso@gmail.com>
2012-03-07 09:47:11 -0800

    Performance improved and IE7 bug fixed

--- heroku.js
+++ heroku.js
@@ -1,27 +1,22 @@
 // Replace each {{app-name}} with the real name application
 jQuery(document).ready(function($) {
-
-   var nameApp = '';
-
+   
    // *** Getting the app name with the current url
    // We get the hostName
    var hostName = window.location.hostname;
 
    // And we parse it with his real application name
-   nameApp = hostName.substr('0',hostName.indexOf(".herokuapp.com"));
+   var nameApp = hostName.substr('0',hostName.indexOf(".herokuapp.com"));
 
-   // // We could define it from HTML
+   // We could define it from HTML
    if (nameApp == '') nameApp = "your-app";
 
-   // // We look into the DOM per each element with {{app-name}}
-   $('*:contains("{{app-name}}")').each(function() {
-      // jQuery :contains selector return every node in its tree. We just want the real container (= just 1 child)    
-      if($(this).children().length < 1) {
-
-         // We replace {{app-name}} for your new Application name and we assigned into itself
-         // $(this).html($(this).html().replace("{{app-name}}",nameApp));
-         $('this').html($(this).html().replace("{{app-name}}",nameApp));
-      }
-   });
+   // To change <title> with IE we have to use document property
+   if ($('title:contains("{{app-name}}")')) {
+      var title = document.title.replace("{{app-name}}",nameApp);
+      document.title = title; 
+   } 
+   
+   $("body *").replaceText("{{app-name}}",nameApp);
 
 });
