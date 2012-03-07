46d945ced981818a6fc195cc526e36badd5a7610
Raul Barroso <ra.barroso@gmail.com>
2012-03-06 16:48:01 -0800

    Inspecting dom tree

--- heroku.js
+++ heroku.js
@@ -1,28 +1,28 @@
 // Replace each {{app-name}} with the real name application
 jQuery(document).ready(function($) {
 
-    var nameApp = '';
+   var nameApp = '';
 
-    // *** Getting the app name with the current url
-    // We get the hostName
-    var hostName = window.location.hostname;
+   // *** Getting the app name with the current url
+   // We get the hostName
+   var hostName = window.location.hostname;
 
-    // And we parse it with his real application name
-    nameApp = hostName.substr('0',hostName.indexOf(".herokuapp.com"));
+   // And we parse it with his real application name
+   nameApp = hostName.substr('0',hostName.indexOf(".herokuapp.com"));
 
-    // // We could define it from HTML
-    if (nameApp == '') nameApp = "your-app";
+   // // We could define it from HTML
+   if (nameApp == '') nameApp = "your-app";
 
-    // // We look into the DOM per each element with {{app-name}}
-    // $('*:contains("{{app-name}}")').each(function() {
+   // // We look into the DOM per each element with {{app-name}}
+   $('*:contains("{{app-name}}")').each(function() {
+      
+   // jQuery :contains selector return every node in its tree. We just want the real container (= just 1 child)    
+   // if($(this).children().length < 1) {
 
-    //     // jQuery :contains selector return every node in its tree. We just want the real container (= just 1 child)    
-    //     if($(this).children().length < 1) {
+   //     // We replace {{app-name}} for your new Application name and we assigned into itself
+   //     $(this).html($(this).html().replace("{{app-name}}",nameApp));
 
-    //         // We replace {{app-name}} for your new Application name and we assigned into itself
-    //         $(this).html($(this).html().replace("{{app-name}}",nameApp));
-            
-    //       }
-    //     });
-    
+   //   }
+   });
+
 });
